9bddb3369a19672d929e6094b921d80c79479b9b
Pranav Gupta <guptapranav360@gmail.com>
2019-02-02 10:04:37 +0530

    No Navigation Present

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [
-    AppComponent  
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    UiModule
+    BrowserModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
