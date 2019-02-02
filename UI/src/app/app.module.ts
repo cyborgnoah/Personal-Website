909ffb6e5c2fcfc529ddb745580aacb69b1c32c8
Pranav Gupta <guptapranav360@gmail.com>
2019-02-02 11:19:35 +0530

    Side Navigation Added

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule, Routes} from "@angular/router";
+import {CustomMaterialModule} from './core/material-module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
 
+
+const appRoutes: Routes = [
+  {path: '',component:AboutComponent,data:{title: 'About Section'}},
+  {path: 'about',component:AboutComponent,data:{title: 'About Section'}},
+  {path: 'experience',component:ExperienceComponent,data:{title: 'Experience Section'}}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AboutComponent,
+    ExperienceComponent
   ],
   imports: [
-    BrowserModule  
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: true }
+    ),
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
